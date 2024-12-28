664208667c3f60f5df2ec2254c88ab5f828d46cf
ricky setiawan suryadi <setiawan.ricky@gmail.com>
2024-12-28 12:10:41 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        port: 5174, // update the port number
+        host: '0.0.0.0', // set to allow connections from all interfaces
+        https: false,
+        open: false,
+      },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 
