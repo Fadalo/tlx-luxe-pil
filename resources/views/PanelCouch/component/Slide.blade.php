c5cfc43ab7c5439bdf7f49e9c13b7443d31e09e3
ricky setiawan suryadi <setiawan.ricky@gmail.com>
2024-12-30 17:54:14 +0700

    update 30-12-2024

--- resources/views/PanelCouch/component/Slide.blade.php
+++ resources/views/PanelCouch/component/Slide.blade.php
@@ -1,7 +1,7 @@
 <div class="card card-style">
     <div class="content">
         <h3 class="font-600">Your Next Session Schedule</h3>
-        <p class="font-11 mt-n2 color-highlight">{{$value['batch_name']}}- {{$value['name']}}</p>
+        <p style="margin-top:0.5em !important" class="font-14 mt-n2 color-highlight">{{$value['batch_name']}}<br><span style="font-size:20px">{{strtoupper($value['name'])}}</span></p>
         
         <div class="float-start">
             <p class="font-10 opacity-80 mb-n1"><i class="far fa-calendar"></i> {{date('F d-Y',strtotime($value['start_datetime']))}}<i class="ms-4 far fa-clock"></i>{{date('H:i A',strtotime($value['start_datetime']))}}</p>
