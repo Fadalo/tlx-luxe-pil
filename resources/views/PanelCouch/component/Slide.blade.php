3d37ed4692e3f19793c7aee988fe17c179734f9a
ricky setiawan suryadi <setiawan.ricky@gmail.com>
2024-12-30 17:55:38 +0700

    update 30-12-2024

--- resources/views/PanelCouch/component/Slide.blade.php
+++ resources/views/PanelCouch/component/Slide.blade.php
@@ -4,7 +4,7 @@
         <p style="margin-top:0.5em !important" class="font-14 mt-n2 color-highlight">{{$value['batch_name']}}<br><span style="font-size:20px">{{strtoupper($value['name'])}}</span></p>
         
         <div class="float-start">
-            <p class="font-10 opacity-80 mb-n1"><i class="far fa-calendar"></i> {{date('F d-Y',strtotime($value['start_datetime']))}}<i class="ms-4 far fa-clock"></i>{{date('H:i A',strtotime($value['start_datetime']))}}</p>
+            <p class="font-10 opacity-80 mb-n1"><i class="far fa-calendar"></i> {{date('F d-Y',strtotime($value['start_datetime']))}}<i class="ms-4 far fa-clock"></i> {{date('H:i A',strtotime($value['start_datetime']))}}</p>
             <p class="font-10 opacity-80"><i class="fa fa-map-marker-alt"></i> LUXE PILATES</p>
         </div>
         
